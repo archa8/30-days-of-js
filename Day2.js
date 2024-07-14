2fae28e85cd598c18fef421eb895345cb8be585d
archa8 <archa.ug22.me@nitp.ac.in>
2024-07-14 11:24:57 +0530

    Day2 first

---
+++ Day2.js
@@ -0,0 +1,71 @@
+let a = 50
+let b = 2
+
+/* Arithmetic Operators */
+// Task 1
+console.log(`Sum is: ${a + b}`)
+
+// Task 2
+console.log(`Difference is: ${a - b}`)
+
+// Task 3
+console.log(`Product is: ${a * b}`)
+
+// Task 4
+console.log(`Quotient is: ${a / b}`)
+
+// Task 5
+console.log(`Remainder is: ${a % b}`)
+
+/* Assignment Operators */
+// Task 6
+b += a
+console.log(`Sum: ${a}`)
+
+// Task 7
+a -= b
+console.log(`Difference: ${b}`)
+
+/* Comparison Operators */
+// Task 8
+bool1 = 67 > 10
+bool2 = 42 < 17
+console.log(`${bool1}, ${bool2}`)
+
+// Task 9
+bool3 = 14 >= 86
+bool4 = 23 <= 23
+console.log(`${bool3}, ${bool4}`)
+
+// Task 10
+bool5 = 10 == 10
+bool6 = 8 != 8
+console.log(`${bool5}, ${bool6}`)
+
+/* Logical Operators */
+// Task 11
+c1 = 77 > 46 && 56 < 23
+console.log(c1)
+
+// Task 12
+c2 = 88 >= 48 || 16 != 16
+console.log(c2)
+
+// Task 13
+c3 = !(99 == 98)
+console.log(c3)
+
+/* Ternary Operator */
+// Task 14
+ter = a > b ? "1st no. is Greater" : "1st no. is Smaller"
+console.log(ter)
+
+//-----------------------------------------------------------
+/* Feature Requests */
+let num1 = 100
+let num2 = 6
+console.log(`Sum= ${num1 + num2}`)
+console.log(`Difference= ${num1 - num2}`)
+console.log(`Product= ${num1 * num2}`)
+console.log(`Quotient= ${num1 / num2}`)
+console.log(`Remainder= ${num1 % num2}`)
