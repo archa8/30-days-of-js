ebdd22cacc3b8cddec453e647e66882ef72ffb5e
archa8 <archa.ug22.me@nitp.ac.in>
2024-08-04 22:54:40 +0530

    Day 23

---
+++ Day23/Task5.js
@@ -0,0 +1,27 @@
+/*
+Activity/Task 5: Word Ladder
+*/
+
+ladderLength = (beginWord, endWord, wordList, stack = [beginWord], seen={beginWord:true}, depth=1,sz = endWord.length) => {
+    const dict = wordList.reduce((m,e)=>(m[e]=true)&&m, {})
+    while(stack.length>0) {
+        let len = stack.length
+        for(let i=0;i<len;i++) {
+            const curr = stack.shift()
+            if (curr==endWord) return depth
+            for(let le=0;le<sz;le++) {
+                const arr = curr.split('')
+                for (let letterIndex=97;letterIndex<123;letterIndex++) {
+                    arr[le] = String.fromCharCode(letterIndex)
+                    const new1 = arr.join('')
+                    if (!seen[new1] && dict[new1]) {
+                        stack.push(new1)
+                        seen[new1] = true
+                    }
+                }
+            }   
+        }
+        depth++
+    }
+    return 0
+  };
