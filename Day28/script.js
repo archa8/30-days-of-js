2c35f9ea692979a3d22eacc42180fd39de45c271
archa8 <archa.ug22.me@nitp.ac.in>
2024-08-11 23:10:37 +0530

    shopping cart layout

---
+++ Day28/script.js
@@ -0,0 +1,9 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+}
+
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+}
