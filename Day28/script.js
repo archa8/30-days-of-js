0b2640940494cd7e7fa72227485ba537cbd50da4
archa8 <archa.ug22.me@nitp.ac.in>
2024-08-12 00:34:00 +0530

    homepage elements added

--- Day28/script.js
+++ Day28/script.js
@@ -1,9 +1,37 @@
 let search = document.querySelector('.search-box');
-
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
+    cart.classList.remove('active');
+    user.classList.remove('active');
 }
 
-document.querySelector('#search-icon').onclick = () => {
-    search.classList.toggle('active');
+let cart = document.querySelector('.cart');
+document.querySelector('#cart-icon').onclick = () => {
+    cart.classList.toggle('active');
+    search.classList.remove('active');
+    user.classList.remove('active');
 }
+
+let user = document.querySelector('.user');
+document.querySelector('#user-icon').onclick = () => {
+    user.classList.toggle('active');
+    search.classList.remove('active');
+    cart.classList.remove('active');
+}
+
+var swiper = new Swiper(".new-arrival", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
