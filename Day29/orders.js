3e66d21aa4bb0bcd1afbb7b4d71420ff52ff8649
archa8 <archa.ug22.me@nitp.ac.in>
2024-08-17 23:49:14 +0530

    Day 29 Tasks

---
+++ Day29/orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+  {
+    productName: "Celine Dion - Let's Talk About Love (CD)",
+    productNumber: '85621',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'Stevie Wonder - Songs in the Key of Life (Vinyl)',
+    productNumber: '85622',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'Thaikkudam Bridge - Namah (CD)',
+    productNumber: '85623',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'A. R. Rahman - Roja (Web)',
+    productNumber: '85624',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'The Rolling Stones - Aftermath (Hi-Res)',
+    productNumber: '85625',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+  },
+  {
+    productName: 'Queen - Sheer Heart Attack (Hi-Res)',
+    productNumber: '85626',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  }
+]
